5b9fdb422500ea13721d418530b59a9ee751bf08
Liangdi <lwan0141@student.monash.edu>
2023-04-07 17:25:23 +1000

    test fetch from db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,23 +1,29 @@
-import bodyParser from "body-parser";
-import express from "express";
-import pg from "pg";
+import bodyParser from "body-parser"
+import express from "express"
+import pg from "pg"
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
-const pool = new pg.Pool();
+const pool = new pg.Pool()
 
-const app = express();
-const port = process.env.PORT || 3333;
+const app = express()
+const port = process.env.PORT || 3333
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(bodyParser.json())
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }))
+app.use(bodyParser.text({ type: "text/html" }))
 
 app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
-});
+  const { rows } = await pool.query("SELECT NOW()")
+  res.send(`Hello, World! The time from the DB is ${rows[0].now}`)
+})
+
+app.get('/jokes', async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM jokes")
+
+  res.json(rows)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
