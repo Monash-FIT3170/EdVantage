190fedfcc3f19a92ab04e6ef124d2881a486dda3
ligsnf <lwan0141@student.monash.edu>
2023-05-02 10:59:18 +1000

    fix stop script to correct url

--- tools/stop.sh
+++ tools/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker-compose -f ./docker/docker-compose.yml down
-docker-compose -f ./docker/docker-compose-postgres.yml down
+docker-compose -f ./tools/docker/docker-compose.yml down
+docker-compose -f ./tools/docker/docker-compose-postgres.yml down
 
 if echo $(docker volume ls) | grep -q "edvantagedb"; then
   docker volume rm edvantagedb
