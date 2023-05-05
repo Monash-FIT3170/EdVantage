69dcf7cbe458f61ff7eb8969f3df3d92e8be4cf4
NickNguyen <hngu0112>
2023-05-05 20:40:10 +1000

    update the quizz section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,19 @@ import Layout from './components/Layout'
 import Public from './components/Public'
 import Login1 from './components/Login1'
 import Login2 from './components/Login2'
+import MultipleQuizz from './components/quizz/MultipleQuizz'
+import {Quizz} from './components/quizz/Quizz.js'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Outlet />} >
-        <Route index element={<Public />} />
-
+        {/* <Route index element={<Public />} />
         <Route path="login" element={<Layout />}>
           <Route path="style1" element={<Login1 />} />
           <Route path="style2" element={<Login2 />} />
-        </Route>
-        
+        </Route> */}
+        <Route index element={<MultipleQuizz />} />
       </Route>
     </Routes>
   )
