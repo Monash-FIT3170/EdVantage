800d56a91e26baa13140210d1051e3bde276cabb
Shareef Abdelal <sabd0011@student.monash.edu>
2023-05-06 14:24:03 +1000

    Create reusable PostgresClient

---
+++ api/src/persistence/PostgresClient.ts
@@ -0,0 +1,31 @@
+import pg from "pg"
+
+var env = process.env.NODE_ENV || 'local'
+var postgresConfig = require('../../postgres/config')[env]
+
+export default class PostgresClient {
+    private pool: pg.Pool;
+
+    constructor() {
+        this.pool = new pg.Pool({
+            database: postgresConfig.database,
+            user: postgresConfig.user,
+            password: postgresConfig.password,
+            host: postgresConfig.host,
+            port: postgresConfig.port,
+            ssl: postgresConfig.sslEnabled,
+            max: 20,
+            idleTimeoutMillis: 1000,
+            connectionTimeoutMillis: 1000
+        });
+    }
+
+    async query(queryText: string, values: any[]): Promise<any> {
+        try {
+            const result = await this.pool.query(queryText, values);
+            return result.rows;
+        } catch (err) {
+            throw err;
+        }
+    }
+}
