b340adfd54b193d206d71c27be06ca6ff93223a0
Josh <j.p.lu@outlook.com>
2023-05-15 18:29:12 +1000

    feat: add sample card component

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Flex } from '@chakra-ui/react';
+import Sidebar from '@/components/Sidebar/Sidebar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <main>
+        <Flex flexDir={'row'}>
+          <Sidebar />
+          <Component {...pageProps} />
+        </Flex>
+      </main>
     </ChakraProvider>
   );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,49 @@
-import Sidebar from '@/components/Sidebar/Sidebar';
+import {
+  Box,
+  Container,
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  CardFooter,
+  ButtonGroup,
+  Button,
+} from '@chakra-ui/react';
 
 export default function Home() {
-  return <Sidebar />;
+  return (
+    <Container h={'100vh'} flexGrow={1} marginInline={0} centerContent>
+      <Card maxW={'container.xl'} my={6} variant={'outline'}>
+        <CardBody>
+          <Image
+            src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="lg">MON0001 Video Lecture</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Stack spacing="3">
+            <Heading size="lg">Transcript</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+          </Stack>
+        </CardFooter>
+      </Card>
+    </Container>
+  );
 }
