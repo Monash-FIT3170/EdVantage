a3e8ade1f1b25c8c2169b826070dace1e3730c4d
Josh <j.p.lu@outlook.com>
2023-05-17 22:11:26 +1000

    fix: quiz types export

--- api/src/routes/QuizTypes.ts
+++ api/src/routes/QuizTypes.ts
@@ -1,39 +1,46 @@
 enum QuestionType {
-    MULTIPLE_CHOICE = 'multiple_choice',
-    SHORT_ANSWER = 'short_answer'
+  MULTIPLE_CHOICE = 'multiple_choice',
+  SHORT_ANSWER = 'short_answer',
 }
 
 type Quiz = {
-    quiz_id: number;
-    title: string;
-    description: string;
-    questions: Question[];
-}
+  quiz_id: number;
+  title: string;
+  description: string;
+  questions: Question[];
+};
 
 type Question = {
-    question_id: number;
-    question: string;
-    question_type: QuestionType;
-    correct_answers: string[];
-    choices?: QuizOption[];
-    //nextQuestionId?: number;
+  question_id: number;
+  question: string;
+  question_type: QuestionType;
+  correct_answers: string[];
+  choices?: QuizOption[];
+  //nextQuestionId?: number;
 };
 
 type QuizQuestion = {
-    quiz_id: number;
-    question_id: number;
-}
+  quiz_id: number;
+  question_id: number;
+};
 
 type QuizAnswer = {
-    question_id: number;
-    answer: string;
+  question_id: number;
+  answer: string;
 };
 
 type QuizOption = {
-    option_id: number;
-    question_id: number;
-    option: string;
-    is_correct: boolean;
-}
+  option_id: number;
+  question_id: number;
+  option: string;
+  is_correct: boolean;
+};
 
-export {QuestionType, Quiz, Question, QuizQuestion, QuizOption, QuizAnswer}
+export type {
+  Question,
+  QuestionType,
+  Quiz,
+  QuizQuestion,
+  QuizOption,
+  QuizAnswer,
+};
