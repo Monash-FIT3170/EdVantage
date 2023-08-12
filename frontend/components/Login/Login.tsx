a4026970c0291fa7c183053cef9ddea50a05ea33
Liangdi <lwan0141@student.monash.edu>
2023-08-12 19:42:17 +1000

    add user role in auth

--- frontend/components/Login/Login.tsx
+++ frontend/components/Login/Login.tsx
@@ -1,11 +1,14 @@
 import { AuthContext } from '@/utils/auth';
+import ApiClient from '@/utils/api-client';
 import { Box, Button, Center, Heading, VStack } from '@chakra-ui/react';
 import { useGoogleLogin } from '@react-oauth/google';
 import { useContext } from 'react';
 import { FaGoogle } from 'react-icons/fa';
 import styles from './Login.module.css';
+import { UserRole } from '@/utils/types';
 
 const Login = () => {
+  const apiClient = new ApiClient();
   const auth = useContext(AuthContext);
   const login = useGoogleLogin({
     scope: 'profile',
@@ -14,15 +17,27 @@ const Login = () => {
         console.log(tokenResponse);
         const response = await fetch(
           'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' +
-            tokenResponse.access_token
+          tokenResponse.access_token
         );
         const userInfo = await response.json();
         console.log(userInfo);
+
+        // Call the login endpoint to create or update the user
+        await apiClient.post(`login`, '', {
+          email: userInfo.email,
+          name: userInfo.name,
+        });
+
+        const roleResponse = await apiClient.get(`auth/userRole`, `email=${userInfo.email}`);
+        const { role } = await roleResponse.json();
+        console.log(role);
+
         auth?.login();
         auth?.setUser({
           name: userInfo.name,
           email: userInfo.email,
           picture: userInfo.picture,
+          role: role as UserRole,
         });
       } catch (err) {
         console.error(err);
