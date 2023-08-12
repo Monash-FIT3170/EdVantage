1ef90f60cb694aaa66c74a60cbb89622dab2947d
Luqmaan <lyur0001@student.monash.edu>
2023-08-12 12:49:08 +1000

    Retrieve files into table and delete with dropdown

--- frontend/components/Uploads/Deletion.tsx
+++ frontend/components/Uploads/Deletion.tsx
@@ -1,22 +1,24 @@
 //Button to delete file from Bucket
 import React, { ChangeEvent } from 'react';
-import { Button, Input } from '@chakra-ui/react';
+import {MenuItem} from '@chakra-ui/react';
 import {deleteFileFromS3} from '../s3Handler'; // Import the S3 handler from the API directory
 import exp from 'constants';
 
-var file:string | undefined;
-export default function DeleteComponent() {
-  const [file, setFile] = React.useState<string>(""); 
+interface DeleteComponentProps {
+  file: string;
+}
+
+export default function DeleteComponent({ file }: DeleteComponentProps) {
   const handleDeleteFile = ()=> {
     if(file){
       const confirmDelete = window.confirm("Are you sure you want to delete this file?")
-      handleFileDelete(file)
+      handleFileDelete()
     }
     else{
       alert("Please select a file first.")
     }
   }
-  const handleFileDelete = (file: string) => {
+  const handleFileDelete = () => {
     // Call the S3 handler function to upload the file to S3
     console.log("Delete")
     deleteFileFromS3(file)
@@ -30,21 +32,12 @@ export default function DeleteComponent() {
         // Handle error cases (if needed)
       });
   }
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFile(event.target.value); // Update the selected file when input changes
-    
-  };
 
   return (
     <div>
-      <Input
-        placeholder='Enter File Name'
-        value={file}
-        onChange={handleInputChange}
-      />
-      <Button margin="5" width="50" placeholder="Save" color="red" onClick={handleDeleteFile}>
+      <MenuItem margin="5" width="50" placeholder="Save" color="red" onClick={handleDeleteFile}>
         Delete
-      </Button>
+      </MenuItem>
     </div>
   );
 }
--- frontend/components/Uploads/Retrieval.tsx
+++ frontend/components/Uploads/Retrieval.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from '@chakra-ui/react';
+import { Button, Menu, MenuButton, MenuList, MenuItem, Table, Thead, Tr, Tbody } from '@chakra-ui/react';
 import { listFilesFromS3 } from '../s3Handler'; // Import the modified listFilesFromS3 function
-
+import DeleteComponent from "@/components/Uploads/Deletion";
 export default function ListFilesComponent() {
   const [files, setFiles] = useState<string[]>([]);
 
@@ -13,28 +13,57 @@ export default function ListFilesComponent() {
     try {
       const fileList = await listFilesFromS3('images/');
       setFiles(fileList);
+      console.log(fileList);
     } catch (error) {
       console.error('Error fetching file list:', error);
     }
   }
 
   return (
-    <div>
-      {files.map((file, index) => (
-        <Button
-          key={index}
-          //make the button span the length of the card
-          width="100%"
-          //make the button purple
-          colorScheme="purple"
-          // add a margin between each button
-            margin="2"
-          onClick={() => console.log(`Clicked on file: ${file}`)}
-          id = {file}
-        >
-          {file}
-        </Button>
-      ))}
-    </div>
+      <div>
+        <Table variant='simple' width={'100%'}>
+          <Thead>
+            <Tr>
+              <th>Video List</th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {files.map((file: string, index) => (
+                <Tr key={index} className="table-row">
+                  <td className="table-cell" onClick={() => console.log(`Clicked on file: ${file}`)}>
+                    {file}
+                  </td>
+                  <td className="table-cell">
+                    <Menu>
+                      <MenuButton as={Button} size="sm" variant="outline">
+                        Options
+                      </MenuButton>
+                      <MenuList>
+                        <DeleteComponent file={file} />
+                        <MenuItem onClick={() => console.log(`Option 2 for file: ${file}`)}>
+                          Option 2
+                        </MenuItem>
+                      </MenuList>
+                    </Menu>
+                  </td>
+                </Tr>
+            ))}
+          </Tbody>
+        </Table>
+
+        <style jsx>{`
+        .table-row {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          border-bottom: 1px solid #e2e8f0;
+          padding: 8px 0;
+        }
+
+        .table-cell {
+          flex: 1;
+        }
+      `}</style>
+      </div>
   );
 }
