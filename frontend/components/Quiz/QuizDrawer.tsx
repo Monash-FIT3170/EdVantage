9a30b3ae0abf17c707a2675fc18be024bb38ebb3
Daniel <57826073+khoodan@users.noreply.github.com>
2023-08-21 18:23:54 +1000

    don't pull / render QuizDrawer until logged in

--- frontend/components/Quiz/QuizDrawer.tsx
+++ frontend/components/Quiz/QuizDrawer.tsx
@@ -17,6 +17,7 @@ import ApiClient from "@/utils/api-client";
 import { Select } from "chakra-react-select";
 import Quiz from "@/components/Quiz/Quiz";
 import QuizResultDrawer from "@/components/Quiz/Results/QuizResultDrawer";
+import { useAuth } from "../AuthProvider";
 
 interface QuizDrawerProps {
   id: string;
@@ -39,6 +40,8 @@ const QuizDrawer = ({ id, drawerState, closeDrawer, openDialog }: QuizDrawerProp
   function openResultsDrawer() { setIsResultsOpen(true); }
   function closeResultsDrawer() { setIsResultsOpen(false); }
 
+  const { isLoggedIn } = useAuth()
+
   const btnRef = useRef(null);
 
   const getQuizzes = (value: any): void => {
@@ -47,43 +50,46 @@ const QuizDrawer = ({ id, drawerState, closeDrawer, openDialog }: QuizDrawerProp
     setUnit(value);
     const apiClient = new ApiClient();
     apiClient
-        .get(`quiz`, "unit_code=" + value.value)
-        .then((res) => res.json())
-        .then((data) => {
-          const quizResponses = [];
-          for (const i in data) {
-            const currQuiz: any = {};
-            currQuiz.label = data[i].title;
-            currQuiz.value = data[i].quiz_id;
-            currQuiz.questions = data[i].questions;
-            quizResponses.push(currQuiz);
-          }
-          setQuizzes(quizResponses);
-          setLoading(false);
-        })
-        .catch((err) => console.error(err));
+      .get(`quiz`, "unit_code=" + value.value)
+      .then((res) => res.json())
+      .then((data) => {
+        const quizResponses = [];
+        for (const i in data) {
+          const currQuiz: any = {};
+          currQuiz.label = data[i].title;
+          currQuiz.value = data[i].quiz_id;
+          currQuiz.questions = data[i].questions;
+          quizResponses.push(currQuiz);
+        }
+        setQuizzes(quizResponses);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
   }
 
   useEffect(() => {
+    if (!isLoggedIn) return;
     setLoading(true);
     const apiClient = new ApiClient();
     apiClient
-        .get(`users/8/units`)
-        .then((res) => res.json())
-        .then((data) => {
-          const unitResponses = [];
-          for (const i in data) {
-            const currUnit: any = {};
-            currUnit.label = data[i].unit_code + ": " + data[i].unit_name;
-            currUnit.value = data[i].unit_code;
-            currUnit.name = data[i].unit_name;
-            unitResponses.push(currUnit)
-          }
-          setUnits(unitResponses);
-          setLoading(false);
-        })
-        .catch((err) => console.error(err));
-  }, []);
+      .get(`users/8/units`)
+      .then((res) => res.json())
+      .then((data) => {
+        const unitResponses = [];
+        for (const i in data) {
+          const currUnit: any = {};
+          currUnit.label = data[i].unit_code + ": " + data[i].unit_name;
+          currUnit.value = data[i].unit_code;
+          currUnit.name = data[i].unit_name;
+          unitResponses.push(currUnit)
+        }
+        setUnits(unitResponses);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, [isLoggedIn]);
+
+  if (!isLoggedIn) return <div />;
 
   return (
     <>
@@ -108,23 +114,23 @@ const QuizDrawer = ({ id, drawerState, closeDrawer, openDialog }: QuizDrawerProp
               <DrawerBody>
                 <Stack spacing={4}>
                   <Select
-                      name="units"
-                      options={units}
-                      placeholder="Select a unit..."
-                      closeMenuOnSelect={true}
-                      onChange={getQuizzes}
-                      value={unit}
-                      size="lg"
+                    name="units"
+                    options={units}
+                    placeholder="Select a unit..."
+                    closeMenuOnSelect={true}
+                    onChange={getQuizzes}
+                    value={unit}
+                    size="lg"
                   />
 
                   {unit && (<Select
-                      name="quizzes"
-                      options={quizzes}
-                      placeholder="Select a quiz..."
-                      closeMenuOnSelect={true}
-                      onChange={setQuiz}
-                      value={quiz}
-                      size="lg"
+                    name="quizzes"
+                    options={quizzes}
+                    placeholder="Select a quiz..."
+                    closeMenuOnSelect={true}
+                    onChange={setQuiz}
+                    value={quiz}
+                    size="lg"
                   />)}
                   <Box>{quiz && <Quiz quiz={quiz} />}</Box>
                 </Stack>
@@ -145,11 +151,11 @@ const QuizDrawer = ({ id, drawerState, closeDrawer, openDialog }: QuizDrawerProp
         </DrawerContent>
 
         {isResultsOpen && (
-            <QuizResultDrawer
-                drawerState={isResultsOpen}
-                closeDrawer={closeResultsDrawer}
-                fetchData={setResultsData}
-            />
+          <QuizResultDrawer
+            drawerState={isResultsOpen}
+            closeDrawer={closeResultsDrawer}
+            fetchData={setResultsData}
+          />
         )}
       </Drawer>
     </>
