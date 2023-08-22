266c44ce0c1dfcc6aa8603b6ab6cc55869d08095
ZM-0 <zmas0005@student.monash.edu>
2023-08-22 14:28:20 +1000

    Formatted quiz listings in quiz manager page

--- frontend/pages/QuizManagerPage.tsx
+++ frontend/pages/QuizManagerPage.tsx
@@ -1,4 +1,9 @@
-import { Box, Button, Heading, Stack } from '@chakra-ui/react';
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
+import {
+  AbsoluteCenter, Box, Button,
+  Card, CardHeader, CardBody, CardFooter, Center,
+  Divider, Heading, IconButton, Spacer, Stack
+} from '@chakra-ui/react';
 import ApiClient from '@/utils/api-client';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
@@ -26,14 +31,45 @@ export default function QuizManagerPage() {
   }, [setQuizzes]);
 
   return (
-    <>
-      <Heading as='h4' size='md'>Quiz Manager</Heading>
-      <Link href='/QuizCreationPage'>
-        <Button>Create Quiz</Button>
+    <Box margin='50px'>
+      <Heading as='h3' size='lg' marginTop='40px' marginBottom='20px'>Quiz Manager</Heading>
+      <Link href='/QuizCreationPage' marginTop='20px' marginBottom='40px'>
+        <Button colorScheme='blue'>Create Quiz</Button>
       </Link>
-      <Stack>
-        {quizzes.map((quiz: Quiz) => <Box key={quiz.id}>{quiz.title}</Box>)}
+      <Box position='relative'>
+        <Divider borderWidth='1px'/>
+        <AbsoluteCenter background='white' padding='10px'>
+          {`${quizzes.length > 0 ? quizzes.length : 'No'} Quizzes`}
+        </AbsoluteCenter>
+      </Box>
+      <Stack spacing='20px' marginTop='40px' marginBottom='40px'>
+        {quizzes.map((quiz: Quiz) => (
+          <Card>
+          <CardBody>
+            <Stack direction='row'>
+              <Stack direction='column'>
+                <Heading as='h4' size='md'>{quiz.title}</Heading>
+                <Box>{`Description: ${quiz.description}`}</Box>
+                <Stack direction='row'>
+                  <Box>{`Question Count: 0`}</Box>
+                  <Box>{`Time Limit: 0 m`}</Box>
+                </Stack>
+              </Stack>
+              <Spacer/>
+              <Center>
+                <Stack direction='row'>
+                  <IconButton aria-label='Edit' icon={<EditIcon/>}/>
+                  <IconButton
+                      aria-label='Delete' icon={<DeleteIcon/>} background='red'
+                      color='white'
+                    />
+                </Stack>
+              </Center>
+            </Stack>
+          </CardBody>
+        </Card>
+        ))}
       </Stack>
-    </>
+    </Box>
   );
 }
