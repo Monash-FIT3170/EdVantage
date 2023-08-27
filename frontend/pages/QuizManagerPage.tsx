9dd525eb71f9783faabbc6a8bb23b3eeeb48bea3
ZM-0 <zmas0005@student.monash.edu>
2023-08-27 22:24:39 +1000

    Fixed print error on quiz manager page

--- frontend/pages/QuizManagerPage.tsx
+++ frontend/pages/QuizManagerPage.tsx
@@ -7,28 +7,21 @@ import { BiEdit } from 'react-icons/bi';
 import { MdDelete } from 'react-icons/md';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-
-
-interface Quiz {
-  title: string;
-  description: string;
-  id: number;
-  questions: any;
-}
+import { Quiz } from '../utils/QuizTypes';
+import PostgresClient from '../utils/PostgresClient';
 
 
 export default function QuizManagerPage() {
   const [quizzes, setQuizzes] = useState<Quiz[]>([]);
 
   useEffect(() => {
+    // const postgresClient = new PostgresClient();
+    // let rows = postgresClient.query('SELECT * FROM AUTHORS');//.then(console.log).catch(console.log);
+    // console.log(`rows = ${rows}`);
+
     const apiClient = new ApiClient();
 
-    apiClient.get('quiz').then(result => result.json()).then(data => {
-      console.log('data = ', data);
-      setQuizzes(data);
-    }).catch(error => {
-      console.log(error);
-    });
+    apiClient.get('quiz').then(result => result.json()).then(setQuizzes).catch(console.log);
   }, [setQuizzes]);
 
   return (
@@ -40,12 +33,12 @@ export default function QuizManagerPage() {
         <Box position='relative'>
           <Divider borderWidth='1px'/>
           <AbsoluteCenter background='white' padding='10px'>
-            {`${quizzes.length > 0 ? quizzes.length : 'No'} Quizzes`}
+            {`${quizzes.length > 0 ? quizzes.length : 'No'} Quiz${1 != quizzes.length ? 'zes' : ''}`}
           </AbsoluteCenter>
         </Box>
         <Stack spacing='20px' marginTop='40px' marginBottom='40px'>
           {quizzes.map((quiz: Quiz) => (
-              <Card key={quiz.id}>
+              <Card key={quiz.quiz_id}>
                 <CardBody>
                   <Stack direction='row'>
                     <Stack direction='column'>
