49ff739e1a5b17c72b47fab350b1815aa44a936a
Shareef Abdelal <sabd0011@student.monash.edu>
2023-08-27 21:53:40 +1000

    Make ChatGPT assistant work

--- frontend/components/ChatAssistant.tsx
+++ frontend/components/ChatAssistant.tsx
@@ -1,42 +1,55 @@
 import React, { useState } from 'react';
 import { Box, Input, VStack, Text, Button } from '@chakra-ui/react';
+import OpenAI from "openai/index";
+import ChatGptUtils from "@/utils/ChatGptUtils";
+
+const openai = new OpenAI({
+  apiKey: ,
+  dangerouslyAllowBrowser: true // Long-term, ensure secret credentials are not exposed with this method
+});
 
 const ChatAssistant = () => {
 
-    const [messages, setMessages] = useState([]);
+    const [messages, setMessages] = useState<any>([]);
+    const [currentMsg, setCurrentMsg] = useState<any>(null);
 
-    const handleSend = () => {
-        // Hardcode user's message
-        const userMessage = "How are you?";
+    const handleSend = async () => {
+      // Hardcode user's message
+      const userMessage = currentMsg;
 
-        // Add the user's message to the chat
-        setMessages([...messages, { sender: 'user', text: userMessage }]);
+      // Add the user's message to the chat
+      setMessages([...messages, {sender: 'user', text: userMessage}]);
+      setCurrentMsg(null);
 
-        // Hardcoded bot response
-        const botResponse = "I'm just a bunch of code, but I'm functioning properly!";
+      // Hardcoded bot response
+      const chatGptUtils = new ChatGptUtils();
+      const requestBody = chatGptUtils.buildChatGptAssistantRequestBody(userMessage)
+      const completion = await openai.chat.completions.create(requestBody);
 
-        // Add bot's response after the user's message
-        setMessages([...messages, { sender: 'user', text: userMessage }, { sender: 'bot', text: botResponse }]);
+
+      const botResponse = completion.choices[0].message['content'];
+
+      // Add bot's response after the user's message
+      setMessages([...messages, {sender: 'user', text: userMessage}, {sender: 'bot', text: botResponse}]);
     };
 
     return (
-        <Box border="1px solid black" borderRadius="md" p={4} width="415px" height="495px" bg="black">
+        <Box border="1px solid black" borderRadius="md" p={4} width="415px" height="495px" >
             <VStack spacing={2} height="80%">
-                {messages.map((message, index) => (
-                    <Text key={index} alignSelf={message.sender === 'bot' ? 'flex-start' : 'flex-end'} color="white">
+                {messages.map((message: any, index: any) => (
+                    <Text key={index} alignSelf={message.sender === 'bot' ? 'flex-start' : 'flex-end'}>
                         {message.text}
                     </Text>
                 ))}
             </VStack>
 
             <Box display="flex" justifyContent="space-between" alignItems="center" mt={4}>
-                <Input 
-                    placeholder="Ask a question." 
-                    variant="outline" 
-                    borderColor="white"
-                    color="white"
+                <Input
+                    placeholder="Ask a question."
+                    variant="outline"
+                    onChange={(e) => setCurrentMsg(e.target.value)}
                 />
-                <Button ml={2} onClick={handleSend} colorScheme="whiteAlpha">Send</Button>
+                <Button ml={2} onClick={handleSend}>Send</Button>
             </Box>
 
         </Box>
