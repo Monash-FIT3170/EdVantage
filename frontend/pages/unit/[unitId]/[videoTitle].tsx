0299ef75ddae4c2c9c100d5a8290095f06459651
Liangdi <lwan0141@student.monash.edu>
2023-08-27 23:11:59 +1000

    add chart to visualizationpane

--- frontend/pages/unit/[unitId]/[videoTitle].tsx
+++ frontend/pages/unit/[unitId]/[videoTitle].tsx
@@ -4,38 +4,7 @@ import React, { useState, useRef } from 'react';
 import type { NextPage } from 'next';
 import { Container, Stack, Box } from '@chakra-ui/react';
 import VideoPane from '@/components/VideoPane';
-import MediaPane from '@/components/MediaPane';
-import type { MediaSource } from '@/utils/types';
-
-const mediaOne: MediaSource[] = [
-  {
-    src: 'https://dkkxc50nup77a.cloudfront.net/images/X32dce7_D48/3.jpg',
-    alt: 'Question about a scary looking limit posted by Pierre Mounir',
-  },
-  {
-    src: 'https://dkkxc50nup77a.cloudfront.net/images/X32dce7_D48/1.png',
-    alt: 'Solve a!b! = a! + b! + c!',
-  },
-  {
-    src: 'https://dkkxc50nup77a.cloudfront.net/images/X32dce7_D48/2.jpg',
-    alt: 'A factorials question where the reader must solve for n',
-  },
-];
-
-const mediaTwo: MediaSource[] = [
-  {
-    src: 'https://dkkxc50nup77a.cloudfront.net/images/X32dce7_D48/4.png',
-    alt: 'Representation of a factorial in a tree form',
-  },
-  {
-    src: 'https://dkkxc50nup77a.cloudfront.net/images/X32dce7_D48/5.png',
-    alt: "Graph comparing the Gamma function to discrete factorial values in the context of Stirling's approximation",
-  },
-  {
-    src: 'https://dkkxc50nup77a.cloudfront.net/images/X32dce7_D48/6.png',
-    alt: 'https://www.desmos.com/calculator/seukyrudt9',
-  },
-];
+import VisualizationPane from '@/components/Visualization/VisualizationPane';
 
 
 const UnitPage: NextPage = () => {
@@ -57,16 +26,7 @@ const UnitPage: NextPage = () => {
           </Box>
           <Box>
             <Stack>
-              <MediaPane
-                title="Interesting Graphs"
-                type={'image'}
-                media={mediaTwo}
-              />
-              <MediaPane
-                title="Discussion Questions"
-                type={'image'}
-                media={mediaOne}
-              />
+              <VisualizationPane currentTime={currentTime} />
             </Stack>
           </Box>
         </Stack>
