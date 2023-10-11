39adddc93467d66286a7ba479aa4f1fdd8be497d
Daniel <57826073+khoodan@users.noreply.github.com>
2023-10-12 01:54:09 +1100

    Quiz creation posts dynamically

--- frontend/components/Quiz/QuizCreation/ShortAnswerCreate.tsx
+++ frontend/components/Quiz/QuizCreation/ShortAnswerCreate.tsx
@@ -1,4 +1,6 @@
+import { useQuizCreationContext } from '@/pages/QuizCreationPage';
 import { Input, Button, Flex } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 type questionCardProp = {
   id: number;
   quizId: number;
@@ -7,11 +9,31 @@ type questionCardProp = {
   correct_answers: string[];
 };
 const ShortAnswerCreate = (prop: questionCardProp) => {
+  const { questionInformation, setQuestionInformation } = useQuizCreationContext();
+
+  const [question, setQuestion] = useState(prop.question || '');
+  const [answer, setAnswer] = useState(prop.correct_answers || []);
+
+  useEffect(() => {
+    const saInfo = {
+      question_id: prop.id,
+      question: question,
+      title: question,
+      type: 'short_answer',
+      question_type: 'short_answer',
+      choices: [],
+      correct_answers: answer,
+      answers: answer.map(ans => ({ answer: ans }))
+    }
+    setQuestionInformation(prop.id, saInfo)
+  }, [question, answer])
+
   return (
       <div style={{ marginBottom: '10px', display: 'flex' }}>
         <div style={{ width: '50%', marginLeft: '10px' }}>
           <Input
               placeholder={prop.question ? prop.question : 'Enter your question'}
+              onChange={(e) => setQuestion(e.target.value)}
               required
           />
           <Input
@@ -20,6 +42,7 @@ const ShortAnswerCreate = (prop: questionCardProp) => {
                     ? prop.correct_answers[0]
                     : 'Enter your correct answer'
               }
+              onChange={(e) => setAnswer([e.target.value])}
               required
           />
         </div>
